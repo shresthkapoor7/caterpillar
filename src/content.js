82dea3b10e93e31637ccedc2ce20d0cc3f88f76d
Shresth Kapoor <shresthkapoor7@gmail.com>
2025-08-03 21:58:30 -0400

    no readme now

--- src/content.js
+++ src/content.js
@@ -1,7 +1,5 @@
-// Content script to help with website access
 console.log('Caterpillar 🐛 content script loaded');
 
-// Listen for messages from background script
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
   if (request.action === 'getPageContent') {
     sendResponse({
